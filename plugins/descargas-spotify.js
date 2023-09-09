995311ca2751026d26fd645df35e6bf6717bd44f
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-09 18:07:19 -0300

    Update descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -2,9 +2,8 @@ import fetch from 'node-fetch'
 import Spotify from "spotifydl-x"
 import fs from 'fs'
 let handler = async(m, { conn, usedPrefix, command, text }) => {
-//let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-//let frep = { contextInfo: { externalAdReply: {title: wm, body: author, sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(gataMenu.getRandom())).buffer() }}}
-if (!text) return await conn.reply(m.chat, `uso incorreto do comando. veja um exemplo de uso:\n_${usedPrefix + command} João Gomes_`, m)
+let frep = { contextInfo: { externalAdReply: {title: wm, body: author, sourceUrl: redesMenu.getRandom(), thumbnail: await(await fetch(gataMenu.getRandom())).buffer() }}}
+if (!text) return await conn.reply(m.chat, `${lenguajeGB.smsMalused2()} ⊱ *${usedPrefix + command} Bellyache*`, m)
 try {
 let resDL = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${text}`)
 let jsonDL = await resDL.json()
@@ -16,15 +15,16 @@ let randomName = getRandom(".mp3")
 const filePath = `./tmp/${randomName}`
 fs.writeFileSync(filePath, spty.audio)
 let spotifyi = `✨ *TÍTULO:*
-» _${spty.data.name}_
+_${spty.data.name}_
 
 🗣️ *ARTISTA:*
 » _${spty.data.artists}_
 
-🌐 *URL*:
+🌐 *LINK DO SPOTIFY*:
 » _${linkDL}_
 
-🎶 *A música está sendo baixada e enviada...*`
+🎶 *Enviando música...*
+${wm}`
 await conn.sendFile(m.chat, spty.data.cover_url, 'error.jpg', spotifyi, m)
 await conn.sendMessage(m.chat, { audio: fs.readFileSync(`./tmp/${randomName}`), fileName: `${spty.data.name}.mp3`, mimetype: "audio/mp4", }, { quoted: m })    
 } catch (e) {
@@ -35,11 +35,12 @@ handler.limit = false
 }}
 handler.command = /^(spotify|music|song)$/i
 handler.limit = 1
+handler.level = 0
 export default handler
 
 const credentials = { clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3', clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009' }
 const spotify = new Spotify.default(credentials)
 async function spotifydl(url) {
 const res = await spotify.getTrack(url).catch(() => {
-return { error: 'erro ao baixar a música.' }})
+return { error: 'O download falhou.' }})
 return { data: res, audio: await spotify.downloadTrack(url) }}
