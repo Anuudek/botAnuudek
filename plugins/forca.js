37061c03b08fa403dd14b2a5fa681fabc4cdabd5
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-11 19:26:16 -0300

    Update forca.js

--- plugins/forca.js
+++ plugins/forca.js
@@ -1,43 +1,34 @@
-import * as sulla from "sulla";
-import request from "request";
-import { JSDOM } from "jsdom";
-import { stripIndents } from "common-tags";
+const { create, Client } = require('@open-wa/wa-automate');
+const request = require("request");
+const { JSDOM } = require("jsdom");
+const { stripIndents } = require("common-tags");
 
-const sessions = [];
+const sessions = {};
 
-function getUserById(id) {
-  return sessions.find((x) => x.id == id);
-}
-
-async function createSession(message) {
-  return await new Promise((resolve) => {
-    let user = getUserById(message.from);
-    if (!user) {
-      user = {
-        id: message.from,
-        type: -1,
-        lastMessage: message.body,
-        forceGame: {
-          answer: "",
-          lastTry: "",
-          points: 0,
-          display: [],
-          confirmation: [],
-          incorrect: [],
-          displayText: null,
-        },
-      };
-      sessions.push(user);
-      console.log(`Add session: ${message.from} message:${message.body}`);
-    }
-    resolve(user);
-  });
+async function createSession(client, message) {
+  if (!sessions[message.from]) {
+    sessions[message.from] = {
+      id: message.from,
+      type: -1,
+      lastMessage: message.body,
+      forceGame: {
+        answer: "",
+        lastTry: "",
+        points: 0,
+        display: [],
+        confirmation: [],
+        incorrect: [],
+        displayText: null,
+      },
+    };
+    console.log(`Add session: ${message.from} message:${message.body}`);
+  }
+  return sessions[message.from];
 }
 
 async function removeSession(user) {
-  const userIndex = sessions.indexOf(user);
-  if (userIndex != -1) {
-    sessions.splice(userIndex, 1);
+  if (sessions[user.id]) {
+    delete sessions[user.id];
     console.log(`removed session id:${user.id}`);
   }
 }
@@ -60,37 +51,131 @@ function retira_acentos(palavra) {
 
 async function initForca(value, client, message, user) {
   try {
-    const res = await fetch(
-      `https://www.palabrasaleatorias.com/palavras-aleatorias.php?fs=1&fs2=${value}&Submit=Nova+palavra`
+    request(
+      `https://www.palabrasaleatorias.com/palavras-aleatorias.php?fs=1&fs2=${value}&Submit=Nova+palavra`,
+      async (err, res) => {
+        if (err) {
+          return console.log(err);
+        }
+        const dom = new JSDOM(res.body);
+        const pageWord = dom.window.document.querySelector("table div")
+          .innerHTML;
+        const word = await retira_acentos(
+          pageWord.toLowerCase().replace(/ /g, "-")
+        );
+        user.forceGame.answer = await word.replace("\n", "");
+        user.forceGame.display = await new Array(word.length - 1).fill(" __ ");
+        await checkPoints(user, client, message);
+      }
     );
-    const html = await res.text();
-    const dom = new JSDOM(html);
-    const pageWord = dom.window.document.querySelector("table div").innerHTML;
-    const word = await retira_acentos(pageWord.toLowerCase().replace(/ /g, "-"));
-    user.forceGame.answer = await word.replace("\n", "");
-    user.forceGame.display = await new Array(word.length - 1).fill(" __ ");
-    await checkPoints(user, client, message);
   } catch (err) {
-    console.log(message.from, `erro: \`${err.message}\`. Tente novamente mais tarde!`);
+    console.log(
+      message.from,
+      `erro: \`${err.message}\`. Tente novamente mais tarde!`
+    );
   }
 }
-
-// Continue adaptando o restante do código da mesma maneira, substituindo o uso de `require` por importações de módulos.
-
-sulla
-  .create("session-marketing", undefined, {
-    headless: true,
-    devtools: false,
-    useChrome: true,
-    debug: false,
-    qr: false,
-  })
-  .then((client) => start(client));
 
 async function checkPoints(user, client, message) {
   // Restante do código da função checkPoints
 }
 
-async function start(client) {
-  // Restante do código da função start
+module.exports = (client) => {
+  client.onMessage(async (message) => {
+    console.log("IsGroup:" + message.isGroupMsg + " Id:" + message.from);
+    if (message.isGroupMsg) {
+      return;
+    }
+    const user = await createSession(client, message);
+
+    user.lastMessage = message.body;
+    console.log(user);
+    if (user.type === -1) {
+      if (message.body.toLowerCase() == "iniciar forca") {
+        await client.sendText(
+          message.from,
+          "Responda com *NUMERO* a categoria que você deseja jogar:\n 1️⃣ - Palavras Aleatórias\n 2️⃣ - Alimentos\n 3️⃣ - Animais\n 4️⃣ - Cores\n 5️⃣ - Corpo Humano\n 6️⃣ - Profissões"
+        );
+        user.type = 0;
+        return;
+      } else {
+        try {
+          await removeSession(user);
+        } catch (e) {
+          //
+        }
+        await client.sendText(
+          message.from,
+          "Desculpe, algo de errado aconteceu comigo, por favor digite *iniciar forca* para iniciar um novo jogo."
+        );
+      }
+    }
+    if (user.type === 0) {
+      console.log("Receive:" + message.body);
+      let selectedValue = -1;
+      switch (message.body) {
+        case "1":
+          selectedValue = 0;
+          break;
+        case "2":
+          selectedValue = 2;
+          break;
+        case "3":
+          selectedValue = 3;
+          break;
+        case "4":
+          selectedValue = 4;
+          break;
+        case "5":
+          selectedValue = 5;
+          break;
+        case "6":
+          selectedValue = 12;
+          break;
+        default:
+          console.log("Invalid option.");
+          break;
+      }
+      if (selectedValue == -1) {
+        return;
+      }
+      await initForca(selectedValue, client, message, user);
+      user.type = 1;
+      return;
+    } else if (user.type == 1) {
+      const choice = message.body.toLowerCase();
+      const word = user.forceGame.answer;
+      user.forceGame.lastTry = choice;
+      if (choice === "end") {
+        return;
+      }
+      console.log(
+       choice equals word: ${choice == word} choice:${choice} ${ choice.length } word:${word}
+);
+if (choice.length > 1 && choice == word) {
+user.forceGame.guessed = true;
+await checkPoints(user, client, message);
+return;
+} else if (word.includes(choice)) {
+user.forceGame.displayText = true;
+for (let i = 0; i < word.length; i++) {
+if (word[i] !== choice) {
+continue;
 }
+user.forceGame.confirmation.push(word[i]);
+user.forceGame.display[i] = word[i];
+}
+await checkPoints(user, client, message);
+return;
+} else {
+user.forceGame.displayText = false;
+if (choice.length === 1) {
+user.forceGame.incorrect.push(choice);
+}
+user.forceGame.points++;
+await checkPoints(user, client, message);
+return;
+}
+}
+});
+};
