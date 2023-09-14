4d4fe802104dea527106d7f5842cd11a81419903
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-14 12:09:09 -0300

    Update propietario(a)-cmd-list.js

--- plugins/propietario(a)-cmd-list.js
+++ plugins/propietario(a)-cmd-list.js
@@ -4,6 +4,6 @@ conn.reply(m.chat, `ღ 𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝘾𝙊𝙈𝘼𝙉𝘿�
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*ღ 𝘾𝙊𝘿𝙄𝙂𝙊:* ${value.locked ? `*(𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘)* ${key}` : key}\n*ღ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊/𝙏𝙀𝙓𝙏𝙊𝙎* ${value.text}`).join('\n\n')}
 `.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
 }
-handler.command = ['listcmd', 'cmdlist']
+handler.command = ['cmdlist']
 handler.rowner = true
 export default handler
