c4280d99f27ba13da72c95ec71705fcf6e598fc3
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-14 11:22:51 -0300

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,38 +1,30 @@
-import translate from '@vitalets/google-translate-api'
-import fetch from "node-fetch"
-let handler = async (m, { text, command, args, usedPrefix }) => {
+import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
+const handler = async (m, {text, command, args, usedPrefix}) => {
 if (!text) throw `utilização incorreta do comando. escreva algo para falar comigo, por exemplo:\n_${usedPrefix + command} olá, bot_`
   try {
-  await conn.sendPresenceUpdate('composing', m.chat)
-  let api = await fetch("https://api.simsimi.net/v2/?text=" + text + "&lc=pt")
-  let resSimi = await api.json()
-  m.reply(resSimi.success)      
-/* conn.sendHydrated(m.chat, `${resSimi.success}`, `𝘼𝙄 | ${wm}`, null, null, null, null, null, [
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m)*/
-  } catch {
-  try {
-  if (text.includes('Olá')) text = text.replace('Olá', 'Hello')
-  if (text.includes('olá')) text = text.replace('olá', 'hello')
-  if (text.includes('OLÁ')) text = text.replace('OLÁ', 'HELLO')    
-  let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text)
-  let resu = await reis.json()  
-  let nama = m.pushName || '1'
-  let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
-  let res = await api.json()
-  let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=pt&dt=t&q=" + res.cnt)
-  let resu2 = await reis2.json()
-  m.reply(resu2[0][0][0])      
-  } catch {  
-  let reisss = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=" + text)
-  let resuuu = await reisss.json()      
-  let res222 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${resuuu[0][0][0]}`)  
-  let json222 = await res222.json()
-  let resulttt = json222.result
-  let lolll = await translate(`${resulttt}`, { to: 'pt', autoCorrect: true })
-  m.reply(lolll.text)      
-  }}
-}
+await conn.sendPresenceUpdate('composing', m.chat)
+const api = await fetch('https://api.simsimi.net/v2/?text=' + text + '&lc=pt');
+const resSimi = await api.json();
+m.reply(resSimi.success);
+} catch {
+try {
+if (text.includes('Olá')) text = text.replace('Olá', 'Hello');
+if (text.includes('olá')) text = text.replace('olá', 'hello');
+if (text.includes('OLÁ')) text = text.replace('OLÁ', 'HELLO');
+const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + text);
+const resu = await reis.json();
+const nama = m.pushName || '1';
+const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+const res = await api.json();
+const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=pt&dt=t&q=' + res.cnt);
+const resu2 = await reis2.json();
+m.reply(resu2[0][0][0]);
+} catch (e) {
+await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'pt' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+}}}
 handler.help = ['simsimi']
 handler.tags = ['General']
 handler.command = ['bot'] 
