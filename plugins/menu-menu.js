d8aeca5640c75b076397619d320ecf377d44304d
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-22 18:11:54 -0300

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -100,7 +100,7 @@ olá, @${m.sender.split`@`[0]} :)
 *->* _.menustickers_
 
 faça bom uso do bot!`.trim()
-await conn.sendFile(m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu, m, { mentions: [m.sender] })
+await conn.sendFile({ mentions: [m.sender] }, m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu)
 	
 } catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
