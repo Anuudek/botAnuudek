6465958cece2feebb0b4535557eb42c1611b8879
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-23 10:15:53 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1160,42 +1160,24 @@ if (!('premium' in user)) user.premium = false
                         typeof plugin.command === 'string' ? // String?
                             plugin.command === command :
                             false
-		
-		//if (text) {
-		//m.reply('*ERROR DE COMANDO*')}
 
-if (!isAccept) {
-continue;
-}
-m.plugin = name;
+
+if (!isAccept)
+continue
+m.plugin = name
+continue
+m.plugin = name
 if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
-const chat = global.db.data.chats[m.chat];
-const user = global.db.data.users[m.sender];
-const botSpam = global.db.data.settings[this.user.jid];
-if (!['owner-unbanchat.js', 'gc-link.js', 'gc-hidetag.js', 'info-creator.js'].includes(name) && chat && chat.isBanned && !isROwner) return; // Except this
-if (name != 'owner-unbanchat.js' && name != 'owner-exec.js' && name != 'owner-exec2.js' && name != 'tool-delete.js' && chat?.isBanned && !isROwner) return;
-   
+let chat = global.db.data.chats[m.chat]
+let user = global.db.data.users[m.sender]
+if (name != 'owner-unbanchat.js' && name != 'owner-exec.js' && name != 'owner-exec2.js' && name != 'tool-delete.js' && chat?.isBanned && !isROwner) return 
 if (m.text && user.banned && !isROwner) {
-if (typeof user.bannedMessageCount === 'undefined') {
-user.bannedMessageCount = 0;
+m.reply('Has sido baneado.')
+return
 }
-if (user.bannedMessageCount < 3) {
-const messageNumber = user.bannedMessageCount + 1;
-const messageText = `❰ ⚠️ ❱ *𝙀𝙎𝙏𝘼𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊/𝘼* ❰ ⚠️ ❱\nAviso (${messageNumber}/3)${user.bannedReason ? `\n*Motivo:* ${user.bannedReason}` : '*Motivo:* Sin especificar (spam)'}
-*👉 Puedes contactar a la propietaria del Bot si crees que se trata de un error (TENER PRUEBAS) para tratar el motivo de tú desbaneo*
+}
+		
 
-👉 ${global.asistencia}
-👉 wa.me/593980586516
-👉 wa.me/593993684821`.trim();
-m.reply(messageText);
-user.bannedMessageCount++;
-} else if (user.bannedMessageCount === 3) {
-user.bannedMessageSent = true;
-} else {
-return;
-}
-return;
-}
 
                 let hl = _prefix 
                 let adminMode = global.db.data.chats[m.chat].modoadmin
