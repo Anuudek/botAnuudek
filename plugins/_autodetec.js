0b6eaefaa78c3c1d7156c01ac9c82277bed1f482
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-26 15:27:42 -0300

    Update _autodetec.js

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -14,11 +14,12 @@ await this.sendMessage(m.chat, { text: `${usuario} trocou a descrição do grupo
 await this.sendMessage(m.chat, { text: `agora *${m.messageStubParameters[0] == 'on' ? 'apenas administradores' : 'todos'}* podem editar as informações do grupo.`, mentions: [m.sender] }, { quoted: m.fkontak })
 } else if (m.messageStubType == 26) {
 await this.sendMessage(m.chat, { text: `o grupo agora está *${m.messageStubParameters[0] == 'on' ? 'fechado. 🔒*' : 'aberto*. 🔓'}\n ${m.messageStubParameters[0] == 'on' ? 'somente os administradores podem enviar mensagens' : 'todos podem enviar mensagens'} no grupo.`, mentions: [m.sender] }, { quoted: m.fkontak })
-} else if (m.messageStubType == 29) {
-await this.sendMessage(m.chat, { text: `@${m.messageStubParameters[0].split`@`[0]} virou administrador do grupo! :D\n\nação realizada por: ${usuario}`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`]/*, mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id)*/ }, { quoted: m.fkontak })
-} else if (m.messageStubType == 30) {
-await this.sendMessage(m.chat, { text: `@${m.messageStubParameters[0].split`@`[0]} não é mais administrador do grupo. :(\n\nação realizada por: ${usuario}`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`]/*, mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id)*/ }, { quoted: m.fkontak })
-} else if (m.messageStubType == 72) {
+}
+//else if (m.messageStubType == 29) {
+//await this.sendMessage(m.chat, { text: `@${m.messageStubParameters[0].split`@`[0]} virou administrador do grupo! :D\n\nação realizada por: ${usuario}`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`]/*, mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id)*/ }, { quoted: m.fkontak })}
+//else if (m.messageStubType == 30) {
+//await this.sendMessage(m.chat, { text: `@${m.messageStubParameters[0].split`@`[0]} não é mais administrador do grupo. :(\n\nação realizada por: ${usuario}`, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`]/*, mentions: (await conn.groupMetadata(m.chat)).participants.map(v => v.id)*/ }, { quoted: m.fkontak })}
+else if (m.messageStubType == 72) {
 await this.sendMessage(m.chat, { text: `${usuario} alterou a duração das mensagens temporárias para: *@${m.messageStubParameters[0]}*`, mentions: [m.sender] }, { quoted: m.fkontak })
 } else if (m.messageStubType == 123) {
 await this.sendMessage(m.chat, { text: `${usuario} *desativou* as mensagens temporárias.`, mentions: [m.sender] }, { quoted: m.fkontak })
