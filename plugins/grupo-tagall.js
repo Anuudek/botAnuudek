be47641ae52091676a1f031ab27d7c538bc7c090
Anuudek <140118333+Anuudek@users.noreply.github.com>
2023-09-26 21:27:21 -0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,10 +4,10 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*-> mensagem:* ${pesan}`
-let teks = `_${wm} • marcando todos do grupo..._\n\n${oi}\n\n`
+let oi = `*mensagem:* ${pesan}`
+let teks = `_${wm} • marcando todos do grupo._\n\n${oi}\n\n`
 for (let mem of participants) {
-teks += `>>> @${mem.id.split('@')[0]}\n`}
+teks += `• @${mem.id.split('@')[0]} • `}
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
